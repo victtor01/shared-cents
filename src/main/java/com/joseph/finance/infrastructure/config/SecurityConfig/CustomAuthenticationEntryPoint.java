475efe3706d97117a06e299f764234e533774580
victtor01 <josevictot.ar@gmail.com>
2025-06-04 23:30:16 -0300

    return json of error 401

---
+++ src/main/java/com/joseph/finance/infrastructure/config/SecurityConfig/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,37 @@
+// CustomAuthenticationEntryPoint.java
+package com.joseph.finance.infrastructure.config.SecurityConfig; // Seu pacote
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule; // Para LocalDateTime
+import com.joseph.finance.shared.exceptions.ErrorInstance;
+import com.joseph.finance.shared.exceptions.ErrorResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+        ErrorResponse errorResponse = new ErrorResponse("UNAUTHORIZED", "Você não tem permissão para acessar", 401);
+
+
+        OutputStream out = response.getOutputStream();
+        objectMapper.writeValue(out, errorResponse);
+        out.flush();
+    }
+}
--- src/main/java/com/joseph/finance/infrastructure/config/SecurityConfig/SecurityConfig.java
+++ src/main/java/com/joseph/finance/infrastructure/config/SecurityConfig/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +16,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -28,6 +30,9 @@ public class SecurityConfig {
     private String origin;
 
     @Autowired
+    private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    @Autowired
     public SecurityConfig(SecurityFilter securityFilter, CorsConfig config) {
         this.securityFilter = securityFilter;
         this.corsConfig = config;
@@ -36,10 +41,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource())).csrf(AbstractHttpConfigurer::disable).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).authorizeHttpRequests(auth -> {
-            auth.requestMatchers("/users").permitAll();
-            auth.requestMatchers("/auth").permitAll();
-            auth.anyRequest().authenticated();
-        }).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class).build();
+                auth.requestMatchers("/users").permitAll();
+                auth.requestMatchers("/auth").permitAll();
+                auth.anyRequest().authenticated();
+            })
+            .exceptionHandling(exceptions -> exceptions
+                .authenticationEntryPoint(customAuthenticationEntryPoint)
+            )
+//            .exceptionHandling(exceptions -> exceptions
+//                    // Define que, para pontos de entrada de autenticação, retorne 401
+//                    .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+//                // O AccessDeniedHandler padrão já retorna 403, mas pode ser customizado aqui também
+//                // .accessDeniedHandler((request, response, accessDeniedException) -> response.setStatus(HttpStatus.FORBIDDEN.value()))
+//            )
+
+            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class).build();
     }
 
     @Bean
